cb3bead7b47c0125c8f01657884e37bad758ebcf
Zaeem khan <khanzaeem010@gmail.com>
2021-07-20 11:51:52 +0500

    Add project files.

---
+++ Send_Email_MVC_Core/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Send_Email_MVC_Core.Models;
+
+namespace Send_Email_MVC_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(EmailModel model)
+        {
+            using (MailMessage mm = new MailMessage(model.Email, model.To))
+            {
+                mm.Subject = model.Subject;
+                mm.Body = model.Body;
+                if (model.Attachment != null)
+                {
+                    if (model.Attachment.Length > 0)
+                    {
+                        string fileName = Path.GetFileName(model.Attachment.FileName);
+                        mm.Attachments.Add(new Attachment(model.Attachment.OpenReadStream(), fileName));
+                    }
+                }
+                mm.IsBodyHtml = false;
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = false;
+                    NetworkCredential NetworkCred = new NetworkCredential(model.Email, model.Password);
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = 587;
+                    smtp.Send(mm);
+                    ViewBag.Message = "Email sent.";
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Send_Email_MVC_Core/Models/EmailModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Send_Email_MVC_Core.Models
+{
+    public class EmailModel
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public IFormFile Attachment { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
